e6ca20ef6c6a898b6fd44e7dac45d04bb3dfe105
Tanisha-2010 <104196498+Tanisha-2010@users.noreply.github.com>
2022-08-27 14:10:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {
+    ///No code for function preload.
+}
+
+function setup() {
+    canvas = createCanvas(640, 430);
+    canvas.position(300, 220);
+}
+
+function draw() {
+
+}
+
+function take_snapshot() {
+    save("image.png");
+}

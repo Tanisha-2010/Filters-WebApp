9255c2e74f15bd77bf4b87559a9e6337f9014e03
Tanisha-2010 <104196498+Tanisha-2010@users.noreply.github.com>
2022-09-02 23:33:04 +0530

    Add files via upload
    added cucumber

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ lip_x = 0;
 lip_y = 0;
 mustache_x = 0;
 mustache_y = 0;
+lens_left_x = 0;
+lens_left_y = 0;
+lens_right_x = 0;
+lens_right_y = 0;
+//middle_x = 0;
+//middle_y = 0;
 
 
 function preload() {
@@ -17,8 +23,21 @@ function draw() {
 
     tint(tint_color);
 
-    image(mustache_, img, mustache_x, mustache_y, 110, 60);
+    image(mustache_img, mustache_x, mustache_y, 110, 60);
     image(lipstick_img, lip_x, lip_y, 110, 50);
+
+    fill(211, 233, 208);
+    stroke(101, 177, 54);
+    circle(lens_left_x, lens_left_y, 90);
+
+    fill(211, 233, 208);
+    stroke(101, 177, 54);
+    circle(lens_right_x, lens_right_y, 90);
+
+    // fill(241, 255, 255);
+    // stroke(190, 255, 155);
+    // rect(middle_x, middle_y, 30)
+
 }
 
 function setup() {
@@ -47,14 +66,30 @@ function takeSnapshot() {
 function gotPoses(result) {
     if (result.length > 0) {
         console.log(result);
+
         console.log("X cordinate of Lip :" + result[0].pose.nose.x);
         console.log("Y coordinate of Lip : " + result[0].pose.nose.y);
+
+        console.log("X coordinate of Mustache : " + result[0].pose.nose.x);
         console.log("Y coordinate of Mustache : " + result[0].pose.nose.y);
-        console.log("Y coordinate of Mustache : " + result[0].pose.nose.y);
-        lip_x = result[0].pose.nose.x + 55;
-        lip_y = result[0].pose.nose.y + 53;
-        mustache_x = result[0].pose.nose.x + 65;
-        mustache_y = result[0].pose.nose.y + 15;
+
+        console.log("X coordinate of Left Lens : " + result[0].pose.leftEye.x);
+        console.log("Y coordinate of Left Lens : " + result[0].pose.leftEye.y);
+
+        console.log("X coordinate of Right Lens : " + result[0].pose.rightEye.x);
+        console.log("Y coordinate of Right Lens : " + result[0].pose.rightEye.y);
+
+        lens_left_x = result[0].pose.leftEye.x + 135;
+        lens_left_y = result[0].pose.leftEye.y + 10;
+
+        lens_right_x = result[0].pose.rightEye.x + 80;
+        lens_right_y = result[0].pose.rightEye.y + 10;
+
+        lip_x = result[0].pose.nose.x + 53;
+        lip_y = result[0].pose.nose.y + 57;
+
+        mustache_x = result[0].pose.nose.x + 60;
+        mustache_y = result[0].pose.nose.y + 20;
     }
 }
 
